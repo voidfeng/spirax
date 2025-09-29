1f7c689d306f7059a5395f70cd2aeae4ff48f242
guanzhao <guanzhao>
2025-09-29 20:18:13 +0800

    新增GitHub Actions工作流以实现通过SSH自动构建和部署，优化NProgress配置和样式，提升用户体验。

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -24,9 +24,11 @@ import 'nprogress/nprogress.css'
 
 // 配置 NProgress
 NProgress.configure({
-  showSpinner: false,
-  trickleSpeed: 200,
-  minimum: 0.1,
+  easing: 'ease', // 动画方式
+  speed: 500, // 递增进度条的速度
+  showSpinner: true, // 是否显示加载ico
+  trickleSpeed: 200, // 自动递增间隔
+  minimum: 0.3, // 初始化时的最小百分比
 })
 
 export default {
